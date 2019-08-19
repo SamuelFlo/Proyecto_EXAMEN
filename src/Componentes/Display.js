cb683f35f0cd3d99988b754a410dfb42ef89e47c
SamuelFlo <samuelflores100@hotmail.com>
2019-08-18 22:27:28 -0600

    exf_6

--- src/Componentes/Display.js
+++ src/Componentes/Display.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Display extends Component {
   render(){
     return(
+      //ENCAPSULADO DISPLAY
       <div className="Display">
       <h1 className="display-1">
         NUTRICION
