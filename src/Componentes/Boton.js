ed1f8e64b8be8172ab488d97f733054f81aa7d6f
SamuelFlo <samuelflores100@hotmail.com>
2019-08-18 22:26:47 -0600

    exf_5

--- src/Componentes/Boton.js
+++ src/Componentes/Boton.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Boton extends Component {
   render(){
     return(
+      //BOTON ENCAPSULADO
       <div className="Boton">
       <button type="submit" className="btn btn-primary">
         Guardar
